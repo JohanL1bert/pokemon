4fd9efa00314c47b57b467768f6e4b3dc61dec40
Johan <grimmerw968@gmail.com>
2022-06-23 14:22:09 +0300

    fix: remove strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,13 @@ import 'app/assets/index.css';
 import { Provider } from 'react-redux';
 import reportWebVitals from 'reportWebVitals';
 import { App } from 'app/App';
-import { store } from 'app/store/store';
+import store from 'app/store/storeConfig';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
