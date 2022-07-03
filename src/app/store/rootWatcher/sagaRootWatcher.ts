eee4bdcd9083383fcefcc3128ee660cb5bc0f222
Johan <grimmerw968@gmail.com>
2022-07-03 19:07:38 +0300

    feat: add saga watchers to main watcher

---
+++ src/app/store/rootWatcher/sagaRootWatcher.ts
@@ -0,0 +1,34 @@
+import { all, call, spawn } from '@redux-saga/core/effects';
+import { getPokemonWatcher } from 'app/store/getPokemon/getPokemon.saga';
+import { getPokemonInfoWatcher } from 'app/store/PokemonInfo/pokemonInfo.saga';
+import { getPokemonImagesWatcher } from 'app/store/PokemonImages/pokemonImages.saga';
+import { getPokemonGrowthRateWatcher } from 'app/store/growthRate/pokemonGrowthRate.saga';
+import { getPokemonAbilitiesWatcher } from 'app/store/pokemonAbilities/pokemonAbilities.saga';
+import { getPokemonCharWatcher } from 'app/store/pokemonCharacteristic/pokemonChar.saga';
+
+function* rootWatcher() {
+  const sagas = [
+    getPokemonWatcher,
+    getPokemonInfoWatcher,
+    getPokemonImagesWatcher,
+    getPokemonGrowthRateWatcher,
+    getPokemonAbilitiesWatcher,
+    getPokemonCharWatcher,
+  ];
+  const retrySagas = sagas.map((saga) => {
+    return spawn(function* () {
+      while (true) {
+        try {
+          yield call(saga);
+        } catch (e) {
+          console.log(e);
+          throw new Error(`${e}, in saga watcher`);
+        }
+      }
+    });
+  });
+
+  yield all(retrySagas);
+}
+
+export { rootWatcher };
