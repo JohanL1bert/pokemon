c0c8fc298add79dd6fed9359222f7e4da7203501
Johan <grimmerw968@gmail.com>
2022-07-03 19:07:10 +0300

    feat: rename reducers

---
+++ src/app/store/reducerRename/renameReducer.ts
@@ -0,0 +1,6 @@
+export { reducer as getPokemonReducer } from 'app/store/getPokemon/getPokemon.reducer';
+export { reducer as getPokemonInfoReducer } from 'app/store/PokemonInfo/pokemonInfo.reducer';
+export { reducer as getPokemonImagesReducer } from 'app/store/PokemonImages/pokemonImages.reducer';
+export { reducer as getPokemonGrowthRateReducer } from 'app/store/growthRate/pokemonGrowthRate.reducer';
+export { reducer as getPokemonAbilitiesReducer } from 'app/store/pokemonAbilities/pokemonAbilities.reducer';
+export { reducer as getPokemonCharReducer } from 'app/store/pokemonCharacteristic/pokemonChar.reducer';
