934a1e6eef7db760b51db82c21ec4d41d4706b9e
Johan <grimmerw968@gmail.com>
2022-07-08 14:58:23 +0300

    feat: add Routes and Route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Header } from 'common/components/Header';
+import { Route, Routes } from 'react-router-dom';
+import { AppRoute } from 'common/enums/app-route.enum';
 import { Home } from 'app/pages/Home';
+import { NotFound } from 'app/pages/NotFound';
 
 export const App = () => {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <Routes>
+      <Route path={AppRoute.ROOT} element={<Home />} />
+      <Route path={AppRoute.NOT_FOUND} element={<NotFound />} />
+    </Routes>
   );
 };
