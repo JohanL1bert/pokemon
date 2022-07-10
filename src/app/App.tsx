3a0a6f0dea4257d3589f08cf7610fa6725cc7034
Johan <grimmerw968@gmail.com>
2022-07-10 17:18:32 +0300

    feat: add nested route for outlet

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import { AppRoute } from 'common/enums/app-route.enum';
 import { Home } from 'app/pages/Home';
 import { NotFound } from 'app/pages/NotFound';
+import { PokemonPage } from 'app/pages/PokemonPage';
+import { Layout } from 'app/pages/Layout';
 
 export const App = () => {
   return (
     <Routes>
-      <Route path={AppRoute.ROOT} element={<Home />} />
-      <Route path={AppRoute.NOT_FOUND} element={<NotFound />} />
+      <Route path={AppRoute.ROOT} element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path={AppRoute.NOT_FOUND} element={<NotFound />} />
+        <Route path={AppRoute.NAME_POKEMON} element={<PokemonPage />} />
+      </Route>
     </Routes>
   );
 };
