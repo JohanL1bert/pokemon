9412d4f40a2769afb23a7f90e9961702fe238026
Johan <grimmerw968@gmail.com>
2022-07-10 17:21:22 +0300

    feat: create pokemon object to put into redux store

--- src/common/helpers/createPokemonObject.ts
+++ src/common/helpers/createPokemonObject.ts
@@ -1,1 +1,9 @@
-export const createPokemonObject = () => {};
+import { IGetPokemonData, IPokemonImagePromise } from 'app/store/getPokemon/interface';
+
+export const createPokemonObject = (
+  requestData: IGetPokemonData,
+  images: Array<IPokemonImagePromise<string>>
+) => {
+  const PokemonImage = images.map((key) => key);
+  return { ...requestData, PokemonImage };
+};
