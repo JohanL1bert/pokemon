9772e271bdda766adeb81a8b1764f13f7acd0618
Johan <grimmerw968@gmail.com>
2022-07-10 17:24:11 +0300

    feat: add parse string for numbers

--- src/common/helpers/parseString.ts
+++ src/common/helpers/parseString.ts
@@ -1,1 +1,18 @@
-export const parseRequestString = () => {};
+import { IGetPokemonData } from 'app/store/getPokemon/interface';
+
+export const parseRequestString = (listOfPokemon: IGetPokemonData) => {
+  const arrayOfPokemonNumber: Array<number> = [];
+  const regexPattern = /[0-9]+\/$/;
+  const listNumbers = listOfPokemon.results;
+
+  Object.values(listNumbers).forEach((key) => {
+    const { url } = key;
+    const matchNumber = url.match(regexPattern)![0];
+    if (matchNumber !== null) {
+      const sliceLastSymbol = matchNumber.slice(0, matchNumber.length - 1);
+      arrayOfPokemonNumber.push(Number(sliceLastSymbol));
+    }
+  });
+
+  return arrayOfPokemonNumber;
+};
