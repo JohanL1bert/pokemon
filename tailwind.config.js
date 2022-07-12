54bca2d5254eafe299fe37240f3890f7e87923db
Johan <grimmerw968@gmail.com>
2022-07-12 18:36:03 +0300

    feat: background image rules to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        backgroundEveryPokemon: "url('app/assets/img/backgroundEveryPokemon.png')",
+      },
+    },
   },
   plugins: [],
 };
