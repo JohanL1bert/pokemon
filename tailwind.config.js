ba6cb4ad5aed02a54f6ec6833d96d96dad42a6b2
Johan <grimmerw968@gmail.com>
2022-07-19 12:54:43 +0300

    feat: add new rules to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     extend: {
       backgroundImage: {
         backgroundEveryPokemon: "url('app/assets/img/backgroundEveryPokemon.png')",
+        backgroundHome: "url('app/assets/img/background_1.png')",
+      },
+      transitionTimingFunction: {
+        'tr-btn-start': 'cubic-bezier(0.19, 1, 0.22, 1)',
+      },
+      outlineOffset: {
+        15: '15px',
+      },
+      colors: {
+        'color-outline': 'rgba(255, 255, 255, 0)',
       },
     },
   },
