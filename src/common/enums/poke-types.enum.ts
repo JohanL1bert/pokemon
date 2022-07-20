05ab25298fef05a1da9db68b60812d735e94e56e
Johan <grimmerw968@gmail.com>
2022-07-20 21:51:31 +0300

    feat: add poke types

---
+++ src/common/enums/poke-types.enum.ts
@@ -0,0 +1,23 @@
+enum PokeTypes {
+  NORMAL = 'normal',
+  FIRE = 'fire',
+  WATER = 'water',
+  GRASS = 'grass',
+  ELECTRIC = 'electric',
+  ICE = 'ice',
+  FIGHTING = 'fighting',
+  POISON = 'poison',
+  GROUND = 'ground',
+  FLYING = 'flying',
+  PSYCHIC = 'psychic',
+  BUG = 'bug',
+  ROCK = 'rock',
+  GHOST = 'ghost',
+  DARK = 'dark',
+  DRAGON = 'dragon',
+  STEEL = 'steel',
+  FAIRY = 'fairy',
+  UNKNOWN = 'unknown',
+}
+
+export { PokeTypes };
