f93f0b62d1e2e90df37ee47336e6599e22a4d30d
Johan <grimmerw968@gmail.com>
2022-07-20 21:50:41 +0300

    feat: add new rules to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,22 @@ module.exports = {
       },
       transitionTimingFunction: {
         'tr-btn-start': 'cubic-bezier(0.19, 1, 0.22, 1)',
+        'tr-btn-arrow': 'cubic-bezier(0.25, 1.7, 0.35, 1.5);',
       },
       outlineOffset: {
         15: '15px',
       },
       colors: {
         'color-outline': 'rgba(255, 255, 255, 0)',
+        'color-arrow': '#111',
+        'color-pseudo-arrow': '#666',
+      },
+      transformOrigin: {},
+      borderWidth: {
+        1: '1px',
+      },
+      height: {
+        '1px': '1px',
       },
     },
   },
