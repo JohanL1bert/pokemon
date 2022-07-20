a3fd83ad929319f39ddc02a07f3596961d170e94
Johan <grimmerw968@gmail.com>
2022-07-20 21:52:04 +0300

    feat : add poke-types const and image for types

---
+++ src/common/const/app.const.ts
@@ -0,0 +1,10 @@
+import imgPokemonNotFound from 'app/assets/img/errorImage/2.png';
+
+export const limitPokemon: number = 32;
+
+export const pathPaginationImages: string =
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+export const imgNotFoundPokemonPagination: string = imgPokemonNotFound;
+
+export const pageUrl = `page=`;
---
+++ src/common/const/image.types.const.ts
@@ -0,0 +1,39 @@
+import normalType from 'app/assets/img/svg/types/Pokémon_Normal_Type_Icon.svg';
+import waterType from 'app/assets/img/svg/types/Pokémon_Water_Type_Icon.svg';
+import steelType from 'app/assets/img/svg/types/Pokémon_Steel_Type_Icon.svg';
+import rockType from 'app/assets/img/svg/types/Pokémon_Rock_Type_Icon.svg';
+import psychicType from 'app/assets/img/svg/types/Pokémon_Psychic_Type_Icon.svg';
+import poisonType from 'app/assets/img/svg/types/Pokémon_Poison_Type_Icon.svg';
+import iceType from 'app/assets/img/svg/types/Pokémon_Ice_Type_Icon.svg';
+import groundType from 'app/assets/img/svg/types/Pokémon_Ground_Type_Icon.svg';
+import grassType from 'app/assets/img/svg/types/Pokémon_Grass_Type_Icon.svg';
+import ghostType from 'app/assets/img/svg/types/Pokémon_Ghost_Type_Icon.svg';
+import flyingType from 'app/assets/img/svg/types/Pokémon_Flying_Type_Icon.svg';
+import fireType from 'app/assets/img/svg/types/Pokémon_Fire_Type_Icon.svg';
+import fightingType from 'app/assets/img/svg/types/Pokémon_Fighting_Type_Icon.svg';
+import fairyType from 'app/assets/img/svg/types/Pokémon_Fairy_Type_Icon.svg';
+import electricType from 'app/assets/img/svg/types/Pokémon_Electric_Type_Icon.svg';
+import dragonType from 'app/assets/img/svg/types/Pokémon_Dragon_Type_Icon.svg';
+import darkType from 'app/assets/img/svg/types/Pokémon_Dark_Type_Icon.svg';
+import bugType from 'app/assets/img/svg/types/Pokémon_Bug_Type_Icon.svg';
+
+export {
+  normalType,
+  waterType,
+  steelType,
+  rockType,
+  psychicType,
+  poisonType,
+  iceType,
+  groundType,
+  grassType,
+  ghostType,
+  flyingType,
+  fireType,
+  fightingType,
+  fairyType,
+  electricType,
+  dragonType,
+  darkType,
+  bugType,
+};
