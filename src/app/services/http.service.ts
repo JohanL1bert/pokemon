aebb57912a5ee1516932382c4519a92a6e9855df
Johan <grimmerw968@gmail.com>
2022-11-14 17:46:38 +0200

    feat: add api interceptors

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,5 +1,13 @@
-export const api = 1;
+import axios from 'axios';
 
 export const httpRequestSetting = {
   responsType: 'arraybuffer',
 };
+
+export const API_URL = `https://pokeapi.co/api/v2/`;
+
+const $api = axios.create({
+  baseURL: API_URL,
+});
+
+export default $api;
