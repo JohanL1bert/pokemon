2045f248af8b397035bcfc94a9a0ec8f1b5caf42
Johan <grimmerw968@gmail.com>
2023-07-27 17:43:32 +0300

    feat: app search serive

--- src/app/services/MainSearchServices.ts
+++ src/app/services/MainSearchServices.ts
@@ -1,6 +1,7 @@
 import { AxiosResponse } from 'axios';
 import { parseUrl } from 'common/helpers/urlParseParams';
 import $api from 'app/services/http.service';
+import { TAxiosMethods } from 'app/services/MainSearchServices.interface';
 
 export class ApiSearchServices {
   static async getPokemonSpecies() {
@@ -9,18 +10,24 @@ export class ApiSearchServices {
     return res;
   }
 
-  static async getPokemonSpeciesByName(name: string) {
-    return $api.get<any>(`/pokemon-species/${name}`);
+  // search by obj url = whole ulr to search, methods optional
+  static async searchByUrl({ url, method = 'get' }: { url: string; method?: TAxiosMethods }) {
+    return $api({ baseURL: url, method });
   }
 
-  static async getPokemonEvolutionChain(/* url: string */): Promise<AxiosResponse<any>> {
-    const url = parseUrl();
-    const res = await $api.get<any>(`/${url}`);
-    console.log(res, 'res');
+  // name - string
+  static async getPokemonSpeciesByName<P, A>(name: P): Promise<AxiosResponse<A>> {
+    return $api.get<A>(`/pokemon-species/${name}`);
+  }
+
+  // url - string
+  static async getRequestParams<P, A>(url: P): Promise<AxiosResponse<A>> {
+    const res = await $api.get<A>(`/${url}`);
     return res;
   }
 
-  static async getPokemonEvolutionChainById(id: number): Promise<AxiosResponse<any>> {
-    return $api.get<any>(`/evolution-chain/${id}`);
+  // id - number
+  static async getPokemonEvolutionChainById<P, A>(id: P): Promise<AxiosResponse<A>> {
+    return $api.get<A>(`/evolution-chain/${id}`);
   }
 }
